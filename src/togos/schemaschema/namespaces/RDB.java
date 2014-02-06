e2e02efcc1a4d6f7cc00e843dcf2d20e3c9d7f6c
Dan Stevens <stevens@earthit.com>
2014-02-05 18:11:11 -0600

    Add RDB/isAutoIncremented predicate.

--- src/togos/schemaschema/namespaces/RDB.java
+++ src/togos/schemaschema/namespaces/RDB.java
@@ -16,7 +16,11 @@ public class RDB
 	public static final Type INDEX       = defineType(NS, "index");
 	public static final Type FOREIGN_KEY = defineType(NS, "foreign key");
 	
+	// Table properties
 	public static final Predicate HAS_FOREIGN_KEY = definePredicate(NS, "has foreign key", FOREIGN_KEY, null);
 	public static final Predicate IS_SELF_KEYED = definePredicate(NS, "is self-keyed", Types.BOOLEAN, "indicates the subject is a class that uses all of its fields as a primary key");
 	public static final Predicate HAS_INDEX = definePredicate(NS, "has index", INDEX, null);
+	
+	// Column properties
+	public static final Predicate IS_AUTO_INCREMENTED = definePredicate(NS, "is auto-incremented", Types.BOOLEAN, null);
 }
