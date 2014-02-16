461d723a352ec23855e485deb3b8d34a4f818162
TOGoS <togos00@gmail.com>
2014-02-16 01:33:21 -0600

    Rename some application class predicates.

--- src/togos/schemaschema/namespaces/Application.java
+++ src/togos/schemaschema/namespaces/Application.java
@@ -10,6 +10,6 @@ public class Application
 	
 	private Application() { }
 	
-	public static final Predicate HAS_DB_TABLE = definePredicate(NS, "has an associated database table", Types.BOOLEAN, "indicates that there is a database table associated with this class");
-	public static final Predicate IS_EXPOSED_VIA_SERVICE = definePredicate(NS, "is exposed via service", Types.BOOLEAN, "indicates that the subject's records should be exposed via services");
+	public static final Predicate HAS_DB_TABLE     = definePredicate(NS, "has an associated database table", Types.BOOLEAN, "indicates that there should be a database table with rows corresponding to instances of a class");
+	public static final Predicate HAS_REST_SERVICE = definePredicate(NS, "has an associated rest service"  , Types.BOOLEAN, "indicates that the subject's records should be exposed via services");
 }
