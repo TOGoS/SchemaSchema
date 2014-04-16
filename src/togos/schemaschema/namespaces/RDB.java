15b1bcfa61fc256b409c1a7108c9404cca742bdd
TOGoS <togos00@gmail.com>
2014-04-16 16:40:10 -0500

    Define a Schema type and nameInDb and isContainedInNamespace predicates in RDB namespace.

--- src/togos/schemaschema/namespaces/RDB.java
+++ src/togos/schemaschema/namespaces/RDB.java
@@ -13,11 +13,16 @@ public class RDB
 {
 	public static final Namespace NS = Namespace.getInstance(NSUtil.SCHEMA_PREFIX+"RDB/");
 	
+	public static final Type SCHEMA      = defineType(NS, "schema");
 	public static final Type INDEX       = defineType(NS, "index");
 	public static final Type SEQUENCE    = defineType(NS, "sequence");
 	public static final Type FOREIGN_KEY = defineType(NS, "foreign key");
 	
 	// Table properties
+	public static final Predicate NAME_IN_DB = definePredicate(NS, "name in database", Types.STRING,
+		"name of the database object that the subject corresponds to; useful when different than what would be inferred based on the subject's canonical name");
+	public static final Predicate IN_NAMESPACE = definePredicate(NS, "is contained in namespace", Types.OBJECT,
+		"namespace (probably a 'schema' object) within which this object is defined");
 	public static final Predicate HAS_FOREIGN_KEY = definePredicate(NS, "has foreign key", FOREIGN_KEY, null);
 	public static final Predicate IS_SELF_KEYED = definePredicate(NS, "is self-keyed", Types.BOOLEAN, "indicates the subject is a class that uses all of its fields as a primary key");
 	public static final Predicate HAS_INDEX = definePredicate(NS, "has index", INDEX, null);
