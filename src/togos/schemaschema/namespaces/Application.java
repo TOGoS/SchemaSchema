0b94b2102b8e70564773871f5a3d2104561db8e1
Dan Stevens <stevens@earthit.com>
2014-08-20 19:40:30 -0500

    Define Application/hasDbColumn predicate.

--- src/togos/schemaschema/namespaces/Application.java
+++ src/togos/schemaschema/namespaces/Application.java
@@ -14,6 +14,7 @@ public class Application
 	private Application() { }
 	
 	public static final Predicate HAS_DB_TABLE     = definePredicate(NS, "has a database table", Types.BOOLEAN, "indicates that there should be a database table with rows corresponding to instances of this class");
+	public static final Predicate HAS_DB_COLUMN    = definePredicate(NS, "has a database column", Types.BOOLEAN, "indicates that there should be a database column corresponding to this field");
 	public static final Predicate HAS_REST_SERVICE = definePredicate(NS, "has a REST service"  , Types.BOOLEAN, "indicates that the this class's instances should be exposed via REST services");
 	public static final Predicate MEMBERS_ARE_PUBLIC = definePredicate(NS, "members are public"  , Types.BOOLEAN, "indicates that instances of this class are not secret and may be visible to the general public");
 	public static final Predicate MEMBERS_ARE_MUTABLE = definePredicate(NS, "members are mutable"  , Types.BOOLEAN, "indicates that instances of this class may be modified while retaining their identity");
