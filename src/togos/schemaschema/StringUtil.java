c4c928ce9e084d921d1f1c2476450377333c91e4
TOGoS <togos00@gmail.com>
2015-12-24 02:07:05 -0600

    Add join and unindent functions.

--- src/togos/schemaschema/StringUtil.java
+++ src/togos/schemaschema/StringUtil.java
@@ -1,5 +1,8 @@
 package togos.schemaschema;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class StringUtil
 {
 	private StringUtil() { }
@@ -13,7 +16,65 @@ public final class StringUtil
 		return sb.toString();
 	}
 	
+	public static final String join( String separator, String[] things ) {
+		if( things.length == 0 ) return "";
+		if( things.length == 1 ) return things[0];
+		StringBuilder sb = new StringBuilder();
+		sb.append(things[0]);
+		for( int i=1; i<things.length; ++i ) {
+			sb.append(separator);
+			sb.append(things[i]);
+		}
+		return sb.toString();
+	}
+	
 	public static final String indent( String prefix, String text ) {
 		return prefix + text.replace("\n", "\n"+prefix);
+	}
+	
+	static final Pattern ALL_WHITESPACE     = Pattern.compile("^\\s*$");
+	static final Pattern LEADING_WHITESPACE = Pattern.compile("^(\\s*)(.*)$");
+	
+	public static final String[] unindent( String[] lines ) {
+		int firstTextLine = -1;
+		int lastTextLine  = -1;
+		
+		for( int i=0; i<lines.length; ++i ) {
+			if( !ALL_WHITESPACE.matcher(lines[i]).matches() ) {
+				if( firstTextLine == -1 ) firstTextLine = i;
+				lastTextLine = i;
+			}
+		}
+		
+		if( firstTextLine == -1 ) return new String[0];
+		
+		String longestConsistentWhitespace = null;
+		
+		for( int i=firstTextLine; i<=lastTextLine; ++i ) {
+			Matcher m = LEADING_WHITESPACE.matcher(lines[i]);
+			if( !m.matches() ) throw new RuntimeException("That should always match!");
+			String lead = m.group(1);
+			if( longestConsistentWhitespace == null ) {
+				longestConsistentWhitespace = lead;
+			} else {
+				int j;
+				for( j=0; j<Math.min(lead.length(), longestConsistentWhitespace.length()); ++j ) {
+					if( lead.charAt(j) != longestConsistentWhitespace.charAt(j) ) {
+						break;
+					}
+				}
+				longestConsistentWhitespace = longestConsistentWhitespace.substring(0, j);
+			}
+		}
+		
+		String[] rez = new String[lastTextLine+1-firstTextLine];
+		for( int i=firstTextLine, r=0; i<=lastTextLine; ++i,++r ) {
+			rez[r] = lines[i].substring(longestConsistentWhitespace.length());
+		}
+		return rez;
+	}
+	
+	public static final String unindent( String text ) {
+		return join("\n",unindent(text.split("\n")));
 	}
 }
--- src/togos/schemaschema/parser/Macros.java
+++ src/togos/schemaschema/parser/Macros.java
@@ -10,6 +10,7 @@ import togos.schemaschema.BaseSchemaObject;
 import togos.schemaschema.Namespace;
 import togos.schemaschema.PropertyUtil;
 import togos.schemaschema.SchemaObject;
+import togos.schemaschema.StringUtil;
 import togos.schemaschema.namespaces.Core;
 import togos.schemaschema.namespaces.Types;
 
@@ -27,6 +28,19 @@ public class Macros
 		return f;
 	}
 	
+	protected static String stringify(SchemaObject o) {
+		Object sv = o.getScalarValue();
+		if( sv != null ) {
+			return sv.toString();
+		} else if( o.getLongName() != null ) {
+			return "<" + o.getLongName() + ">";
+		} else if( o.getName() != null ) {
+			return "<" + o.getName() + ">";
+		} else {
+			return "<null>";
+		}
+	}
+	
 	public static final Function CONCAT = defun("concat", new Function() {
 		@Override public SchemaObject apply(SchemaInterpreter interp, SchemaObject[] arguments, List<SourceLocation> trace) throws ScriptError {
 			if( arguments.length == 1 ) return arguments[0];
@@ -34,20 +48,42 @@ public class Macros
 			// Really this should also work for non-string blobs, too.
 			// But for now there probably aren't any.
 			StringBuilder sb = new StringBuilder();
-			for( SchemaObject o : arguments ) {
-				Object sv = o.getScalarValue();
-				if( sv != null ) {
-					sb.append(sv.toString());
-				} else if( o.getLongName() != null ) {
-					sb.append("<"+o.getLongName()+">");
-				} else if( o.getName() != null ) {
-					sb.append("<"+o.getName()+">");
-				} else {
-					sb.append("<null>");
-				}
+			for( SchemaObject o : arguments ) sb.append(stringify(o));
+			
+			return BaseSchemaObject.forScalar(sb.toString(), trace.get(trace.size()-1));
+		}
+	});
+	
+	public static final Function JOIN = defun("join", new Function() {
+		@Override public SchemaObject apply(SchemaInterpreter interp, SchemaObject[] arguments, List<SourceLocation> trace) throws ScriptError {
+			if( arguments.length == 1 ) return BaseSchemaObject.forScalar("", trace.get(trace.size()-1));
+			
+			// TODO: If arguments are lists, recursively join their contents
+			
+			if( arguments.length == 2 ) return arguments[1];
+			
+			String sep = stringify(arguments[1]);
+			
+			// Really this should also work for non-string blobs, too.
+			// But for now there probably aren't any.
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append(stringify(arguments[2]));
+			for( int i=2; i<arguments.length; ++i ) {
+				sb.append(sep);
+				sb.append(stringify(arguments[i]));
 			}
 			
 			return BaseSchemaObject.forScalar(sb.toString(), trace.get(trace.size()-1));
+		}
+	});
+	
+	/** Handy for multi-line comments? */
+	public static final Function UNINDENT = defun("unindent", new Function() {
+		@Override public SchemaObject apply(SchemaInterpreter interp, SchemaObject[] arguments, List<SourceLocation> trace) throws ScriptError {
+			if( arguments.length != 1 ) throw new ScriptError("Unindent takes only a single argument", trace);
+			
+			return BaseSchemaObject.forScalar(StringUtil.unindent(stringify(arguments[0])));
 		}
 	});
 }
